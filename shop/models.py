f7a8c34537b17fc58f3213b613e391810f9a86bf
Ivan Mironov <comrade.miron@gmail.com>
2022-07-06 16:44:43 +0300

    add models

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Заголовок')
+    description = models.TextField(verbose_name = 'Описание')
+    image = models.ImageField(upload_to='shop/')
+    price = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='Цена')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    slug = models.SlugField(max_length=100, unique=True, verbose_name='URL')
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Имя категории')
+
+class Payment(models.Models):
+    user = model.models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='Цена')
+    time = models.DateField(auto_now_add=True)
+    comment = models.TextField()
+
+class Order(model.Models):
+    user =  model.models.ForeignKey(User, on_delete=models.CASCADE)
+    
--- shop/scraping.py
+++ shop/scraping.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-
+import re
 # from main.settings import URL_SCRAPING_DOMAIN, URL_SCRAPING
 
 def scraping():
@@ -14,23 +14,30 @@ def scraping():
     text = resp.text
 
     soup = BeautifulSoup(text, 'html.parser')
-    blocks = soup.select('.productionItem') # получаем список товаров по селектору productionItem
+    blocks = soup.select(".productionItem") # получаем список товаров по селектору productionItem
     
+   
     data_list=[]
+
+
     for block in blocks:
         data = {}
+
         name = block.select_one('.title').get_text(strip=True) # имя по селектору title, strip=True - удаляем пробелы
         data['name'] = name
 
         description = block.select_one('.desc').get_text(strip=True)
         data['description'] = description
-        
+
         image = img_url + block.select_one('img')['src'] # url изображения, по img и атрибуту scr
         data['image'] = image
 
-        price = 0
+        price = 'По запросу'
         data['price'] = price
 
+
         data_list.append(data)
-    print(data_list) 
+    
+        print(data)
+        
 scraping()
